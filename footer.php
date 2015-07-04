3a6dbad55ab88fd83725c2a94988ef795f7f3d89
Gaurav Pareek <me@grv.rocks>
2015-07-04 23:08:54 +0530

    Updated footer text

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info container">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'relativity' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'relativity' ), 'WordPress' ); ?></a>
 			<br />
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'relativity' ), 'relativity', '<a href="http://magikpress.com/" rel="designer">MagikPress</a>' ); ?>
+			<?php printf( esc_html__( 'Built with %1$s at %2$s.', 'relativity' ), '<big>&hearts;</big>', '<a href="http://magikpress.com/" rel="designer">MagikPress</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
