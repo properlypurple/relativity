a4f731f01f49b6d84dc002616b2d104e59a9f498
Gaurav Pareek <me@grv.rocks>
2015-07-04 22:27:45 +0530

    Remove code for widget area

--- functions.php
+++ functions.php
@@ -85,26 +85,6 @@ function relativity_content_width() {
 add_action( 'after_setup_theme', 'relativity_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link http://codex.wordpress.org/Function_Reference/register_sidebar
- */
-/*
-function relativity_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'relativity' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-}
-add_action( 'widgets_init', 'relativity_widgets_init' );
-*/
-
-/**
  * Enqueue scripts and styles.
  */
 function relativity_scripts() {
