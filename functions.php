7b629b8a6c829922edbd3ed78da04c6b87651962
Gaurav Pareek <me@grv.rocks>
2015-12-13 17:06:06 +0530

    Refactored the custom header feature. Removed a bunch of leftover code from underscores

--- functions.php
+++ functions.php
@@ -114,39 +114,7 @@ add_action( 'wp_enqueue_scripts', 'relativity_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-function relativity_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'relativity_custom_header_args', array(
-		'default-image'          => relativity_get_default_header_image(),
-		'width'                  => 150,
-		'height'                 => 150,
-		'flex-height'            => true,
-		'flex-width'             => true,
-	) ) );
-}
-add_action( 'after_setup_theme', 'relativity_custom_header_setup' );
-
-/**
- * Implement the Custom Avatar Header feature.
- */
-function relativity_get_default_header_image() {
-
-	// Get default from Discussion Settings.
-	$default = get_option( 'avatar_default', 'mystery' ); // Mystery man default.
-	if ( 'mystery' == $default ) {
-		$default = 'mm';
-	} elseif ( 'gravatar_default' == $default ) {
-		$default = '';
-	}
-
-	$protocol = ( is_ssl() ) ? 'https://secure.' : 'http://';
-	$url = sprintf( '%1$sgravatar.com/avatar/%2$s/', $protocol, md5( get_option( 'admin_email' ) ) );
-	$url = add_query_arg( array(
-		's' => 150,
-		'd' => urlencode( $default ),
-	), $url );
-
-	return esc_url_raw( $url );
-} // relativity_get_default_header_image
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -17,110 +17,38 @@
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses relativity_header_style()
- * @uses relativity_admin_header_style()
- * @uses relativity_admin_header_image()
+ * @uses relativity_get_default_header_image()
  */
 function relativity_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'relativity_custom_header_args', array(
-		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-image'          => relativity_get_default_header_image(),
+		'width'                  => 150,
+		'height'                 => 150,
 		'flex-height'            => true,
-		'wp-head-callback'       => 'relativity_header_style',
-		'admin-head-callback'    => 'relativity_admin_header_style',
-		'admin-preview-callback' => 'relativity_admin_header_image',
+		'flex-width'             => true,
 	) ) );
 }
 add_action( 'after_setup_theme', 'relativity_custom_header_setup' );
 
-if ( ! function_exists( 'relativity_header_style' ) ) :
-	/**
-	 * Styles the header image and text displayed on the blog
-	 *
-	 * @see relativity_custom_header_setup().
-	 */
-	function relativity_header_style() {
-		$header_text_color = get_header_textcolor();
+/**
+ * Implement the Custom Avatar Header feature.
+ */
+function relativity_get_default_header_image() {
 
-		// If no custom options for text are set, let's bail
-		// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value.
-		if ( HEADER_TEXTCOLOR == $header_text_color ) {
-			return;
-		}
-
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-		<?php
-		// Has the text been hidden?
-		if ( 'blank' == $header_text_color ) :
-		?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
-			}
-		<?php
-			// If the user has set a custom color for the text use that.
-			else :
-		?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
-		</style>
-		<?php
+	// Get default from Discussion Settings.
+	$default = get_option( 'avatar_default', 'mystery' ); // Mystery man default.
+	if ( 'mystery' == $default ) {
+		$default = 'mm';
+	} elseif ( 'gravatar_default' == $default ) {
+		$default = '';
 	}
-endif; // End relativity_header_style.
 
-if ( ! function_exists( 'relativity_admin_header_style' ) ) :
-	/**
-	 * Styles the header image displayed on the Appearance > Header admin panel.
-	 *
-	 * @see relativity_custom_header_setup().
-	 */
-	function relativity_admin_header_style() {
-	?>
-		<style type="text/css">
-			.appearance_page_custom-header #headimg {
-				border: none;
-			}
-			#headimg h1,
-			#desc {
-			}
-			#headimg h1 {
-			}
-			#headimg h1 a {
-			}
-			#desc {
-			}
-			#headimg img {
-			}
-		</style>
-	<?php
-	}
-endif; // End relativity_admin_header_style.
+	$protocol = ( is_ssl() ) ? 'https://secure.' : 'http://';
+	$url = sprintf( '%1$sgravatar.com/avatar/%2$s/', $protocol, md5( get_option( 'admin_email' ) ) );
+	$url = add_query_arg( array(
+		's' => 150,
+		'd' => urlencode( $default ),
+	), $url );
 
-if ( ! function_exists( 'relativity_admin_header_image' ) ) :
-	/**
-	 * Custom header image markup displayed on the Appearance > Header admin panel.
-	 *
-	 * @see relativity_custom_header_setup().
-	 */
-	function relativity_admin_header_image() {
-	?>
-		<div id="headimg">
-			<h1 class="displaying-header-text">
-				<a id="name" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>" onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-			</h1>
-			<div class="displaying-header-text" id="desc" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>"><?php bloginfo( 'description' ); ?></div>
-			<?php if ( get_header_image() ) : ?>
-			<img src="<?php header_image(); ?>" alt="">
-			<?php endif; ?>
-		</div>
-	<?php
-	}
-endif; // End relativity_admin_header_image.
+	return esc_url_raw( $url );
+} // relativity_get_default_header_image
