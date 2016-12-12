17c6ede48b17903436d4192a8afcefb945169919
Gaurav Pareek <me@grv.rocks>
2016-12-12 21:59:22 +0530

    more tweaks to links

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
 		<div class="site-info container">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'relativity' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'relativity' ), 'WordPress' ); ?></a>
 			<br />
-			<?php printf( esc_html__( 'Built with %1$s at %2$s.', 'relativity' ), '<big>&hearts;</big>', '<a href="http://magikpress.com/" rel="designer">MagikPress</a>' ); ?>
+			<?php printf( esc_html__( '%1$s by %2$s.', 'relativity' ), '<a href="https://wordpress.org/themes/relativity">Relativity</a>', '<a href="http://magikpress.com/" rel="designer">MagikPress</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
